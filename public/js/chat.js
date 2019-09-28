76578490d13410235aea6367b1de88ac534bb749
Vishwaswaroop Bennur <vishwaswaroop2000@gmail.com>
2019-09-28 19:14:57 +0530

    Init commit

---
+++ public/js/chat.js
@@ -0,0 +1,99 @@
+const socket = io()
+const $messages = document.querySelector('#messages')
+//Elements
+$('#message-form').submit(() => {
+  event.preventDefault()
+  const message = event.target.elements.message.value
+  $('button').attr('disabled', 'disabled')
+  socket.emit('sendMessage', message, (error) => {
+    $('button').removeAttr('disabled')
+    $('input').val('')
+    $('input').focus()
+    if (error)
+      return console.log(error)
+  })
+})
+
+const {
+  username,
+  room
+} = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+})
+
+const autoscroll = () => {
+  //New message element
+  const $newMessage = $messages.lastElementChild
+  //Height of the new message
+  const newMessageStyles = getComputedStyle($newMessage)
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+  //Visible Height
+  const visibleHeight = $messages.offsetHeight
+  //Height of message container
+  const containerHeight = $messages.scrollHeight
+  const scrollOffset = Math.ceil($messages.scrollTop + visibleHeight)
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight
+  }
+}
+
+socket.on('message', (message) => {
+  const html = Mustache.render($('#message-template').html(), {
+    username: message.username,
+    message: message.text,
+    createdAt: moment(message.createdAt).format('h:mm A')
+  })
+  //so what happens is Moustache.render will make the acutal message be render at index.html
+  //then this is append to the messages div  below
+
+  $('#messages').append(html)
+  autoscroll()
+})
+
+socket.on('locationMessage', (urlObject) => {
+  const html = Mustache.render($('#location-message-template').html(), {
+    username: urlObject.username,
+    url: urlObject.location,
+    createdAt: moment(urlObject.createdAt).format('h:mm A')
+  })
+
+  $('#messages').append(html)
+  autoscroll()
+})
+
+socket.on('roomData', ({
+  room,
+  users
+}) => {
+  const html = Mustache.render($('#sidebar-template').html(), {
+    room,
+    users
+  })
+  $('#sidebar').empty()
+  $('#sidebar').append(html)
+})
+$('#send-location').click(() => {
+  if (!navigator.geolocation)
+    return alert('Geolocation is not supported by your browser.')
+  $('#send-location').attr('disabled', 'disabled')
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('sendLocation', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    }, () => {
+      $('#send-location').removeAttr('disabled')
+    })
+  })
+
+})
+
+socket.emit('join', {
+  username,
+  room
+}, (error) => {
+  if (error) {
+    alert(error)
+    location.href = '/'
+  }
+})
---
+++ src/index.js
@@ -0,0 +1,90 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+const Filter = require('bad-words')
+const {
+  generateLocationMessage,
+  generateMessage
+} = require('./utils/messages')
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+} = require('./utils//users')
+
+const app = express()
+//'create a new server' with the 'express app'slication
+const server = http.createServer(app)
+const io = socketio(server) // create a socket io server by passing a raw http server.
+//This express does not allow us the access of the raw htttp server thats why refactoring is done here
+
+const port = process.env.PORT || 3000
+
+const publicDirectory = path.join(__dirname, '../public')
+
+//app.use sets up a middle ware - express.static which is used to serve up static files in the `public directory`
+app.use(express.static(publicDirectory))
+
+let count = 0
+
+io.on('connection', (socket) => {
+  console.log("New web socket connection")
+
+  socket.on('join', (options, callback) => {
+    const {
+      error,
+      user
+    } = addUser({
+      id: socket.id,
+      ...options
+    })
+
+    if (error) {
+      return callback(error)
+    }
+
+
+    socket.join(user.room) //Makes the socket get a room through which it can get functions
+    socket.emit('message', generateMessage('Admin', 'Welcome!'))
+    socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`))
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room)
+    })
+    callback()
+  })
+
+  socket.on('sendMessage', (message, callback) => {
+    user = getUser(socket.id)
+    const filter = new Filter()
+    if (filter.isProfane(message))
+      return callback('Profanity is not allowed')
+    io.to(user.room).emit('message', generateMessage(user.username, message))
+    callback()
+  })
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id)
+    if (user) {
+      io.to(user.room).emit('message', generateMessage(`${user.username} has left`))
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      })
+    }
+  })
+
+  socket.on('sendLocation', (coords, callback) => {
+    const user = getUser(socket.id)
+    io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+    callback()
+  })
+})
+//But how will it know that the client is connected to the server? That is why it has client version of the library,
+//which needs to be included in the client version of the JS
+
+server.listen(port, () => {
+  console.log(`server is up on port ${port}`)
+})
